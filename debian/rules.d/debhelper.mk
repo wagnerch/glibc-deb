c6a3b149855fbf85691dab3b6ce0433d65677859
Jeff Bailey <jbailey@debian.org>
2003-09-27 23:25:39 +0000

    More nptl work.  Builds packages now.  Pitty the packages don't install
    svn path=/branches/nptl/; revision=411

--- debian/rules.d/debhelper.mk
+++ debian/rules.d/debhelper.mk
@@ -9,30 +9,63 @@ $(patsubst %,$(stamp)binaryinst_%,$(DEB_ARCH_REGULAR_PACKAGES) $(DEB_INDEP_REGUL
 	dh_installdebconf -p$(curpass)
 	dh_installchangelogs -p$(curpass)
 	dh_installinit -p$(curpass)
+	dh_installdocs -p$(curpass) 
 	dh_link -p$(curpass)
 
-	dh_strip -p$(curpass)
+	if [ ! $(NOSTRIP_$(curpass)) ]; then \
+	  dh_strip -p$(curpass); \
+	fi
+
 	dh_compress -p$(curpass)
 	dh_fixperms -p$(curpass)
 	dh_makeshlibs -p$(curpass)
 
 	dh_installdeb -p$(curpass)
-	dh_shlibdeps -p$(curpass)
+	# dh_shlibdeps -p$(curpass)
 	dh_gencontrol -p$(curpass)
 	dh_md5sums -p$(curpass)
 	dh_builddeb -p$(curpass)
 
 	touch $@
 
+#Ugly kludge:
+# I'm running out of time to get this sorted out properly.  Basically
+# the problem is that nptl is like an optimised library, but not quite.
+# So we'll filter it out of the passes list and deal with it specifically.
+#
+# Ideally, there should be some way of having an optimisation pass and
+# say "include this in the main library" by setting a variable.
+# But after 10 hours of staring at this thing, I can't figure it out.
+
+OPT_PASSES = $(filter-out libc nptl,$(GLIBC_PASSES))
+NPTL = $(filter nptl,$(GLIBC_PASSES))
+
 debhelper: $(stamp)debhelper
 $(stamp)debhelper:
 	for x in `find debian/debhelper.in -type f -maxdepth 1`; do \
-	 y=debian/`basename $$x`; \
-	 z=`echo $$y | sed -e 's#/libc#/$(libc)#'`; \
-	 cp $$x $$z; \
-	 sed -e 's#TMPDIR#debian/tmp-libc#' -i $$z; \
-	 sed -e 's#DEB_SRCDIR#$(DEB_SRCDIR)#' -i $$z; \
+	  y=debian/`basename $$x`; \
+	  z=`echo $$y | sed -e 's#/libc#/$(libc)#'`; \
+	  cp $$x $$z; \
+	  sed -e "s#TMPDIR#debian/tmp-libc#" -i $$z; \
+	  sed -e "s#DEB_SRCDIR#$(DEB_SRCDIR)#" -i $$z; \
 	done  
+
+	for x in $(OPT_PASSES); do \
+	  z=debian/$(libc)-$$x.install; \
+	  cp debian/debhelper.in/libc-opt.install $$z; \
+	  sed -e "s#TMPDIR#debian/tmp-$$x#" -i $$z; \
+	  sed -e "s#DEB_SRCDIR#$(DEB_SRCDIR)#" -i $$z; \
+	  sed -e "s#DESTLIBDIR#$$x#" -i $$z; \
+	done
+
+	for x in $(NPTL); do \
+	  z=debian/$(libc).install; \
+	  cat debian/debhelper.in/libc-opt.install >>$$z; \
+	  sed -e "s#TMPDIR#debian/tmp-$$x#" -i $$z; \
+	  sed -e "s#DEB_SRCDIR#$(DEB_SRCDIR)#" -i $$z; \
+	  sed -e "s#DESTLIBDIR#$$x#" -i $$z; \
+	done
+
 
 debhelper-clean:
 	dh_clean 
@@ -49,3 +82,5 @@ debhelper-clean:
 	rm -f debian/*.init
 	rm -f debian/*.config
 	rm -f debian/*.templates
+
+	rm -f $(stamp)binaryinst*
