96ed9bc933c6a339577c7759070284afb9821758
Daniel Jacobowitz <dan@debian.org>
2003-10-12 19:18:39 +0000

    - Fix shlibs files to include a version again.
    svn path=/branches/nptl/; revision=466

--- debian/sysdeps/sparc.mk
+++ debian/sysdeps/sparc.mk
@@ -6,6 +6,7 @@ sparc64_configure_target=sparc64-linux
 sparc64_CC = $(BUILD_CC) -m64
 sparc64_extra_config_options = $(extra_config_options) --disable-profile --enable-omitfp
 sparc64_add-ons = linuxthreads $(add-ons)
+libc6-sparc64_shlib_dep = libc6-sparc64 (>= $(shlib_dep_ver))
  
 sparcv9_MIN_KERNEL_REQUIRED = 2.4.18
 sparcv9_configure_target=sparcv9-linux
