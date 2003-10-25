8553c04336785f7a303eecff45030d7efca6a042
Daniel Jacobowitz <dan@debian.org>
2003-10-25 00:53:20 +0000

    - Switch the i686 optimized libraries to use NPTL, and update their       description.
    svn path=/branches/nptl/; revision=503

--- debian/sysdeps/i386.mk
+++ debian/sysdeps/i386.mk
@@ -1,8 +1,6 @@
 GLIBC_PASSES += nptl i686
 DEB_ARCH_REGULAR_PACKAGES += libc6-i686
 
-i686_MIN_KERNEL_SUPPORTED = 2.4.18
-
 # NPTL requires at least i486 assembly.  We don't need to take
 # special measures for i386 systems, since Debian kernel images now
 # emulate the missing instructions on the i386.
@@ -13,13 +11,13 @@ nptl_LIBDIR = /tls
 
 # We use -march=i686 and glibc's i686 routines use cmov, so require it.
 # A Debian-local glibc patch adds cmov to the search path.
+# The optimized libraries also use NPTL!
+i686_add-ons = nptl $(add-ons)
 i686_configure_target=i686-linux
 i686_configure_build=i686-linux
 i686_extra_cflags = -march=i686 -mcpu=i686
-i686_LIBDIR = /i686/cmov
-
-i686_extra_config_options = $(extra_config_options) --disable-profile --enable-omitfp --with-tls --without-__thread
+i686_LIBDIR = tls/i686/cmov
+i686_MIN_KERNEL_SUPPORTED = 2.6.0
+i686_extra_config_options = $(extra_config_options) --disable-profile --enable-omitfp --with-tls --with-__thread
 
 libc_extra_config_options = $(extra_config_options) --with-tls --without-__thread
-
-i686_add-ons = linuxthreads $(add-ons)
