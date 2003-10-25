1425ccec36a5652f54e3877c24fddb9c07059f8a
Daniel Jacobowitz <dan@debian.org>
2003-10-25 19:29:08 +0000

    Redo last patch to actually work.
    svn path=/branches/nptl/; revision=529

--- debian/sysdeps/depflags.mk
+++ debian/sysdeps/depflags.mk
@@ -4,7 +4,13 @@ depflags = libc=$(libc) glibc=glibc \
 	  perl debian/sysdeps/depflags.pl
 
 libc_control_flags = $(shell $(depflags) libc)
-libc_dev_control_flags = $(shell $(depflags) libc_dev) $(libc) (= $(DEB_VERSION))
+libc_dev_control_flags = $(shell $(depflags) libc_dev)
+
+# If there's a -DDepends for libc-dev, add this to it.  If there isn't
+# then the control file's depends line (which contains this) will be used.
+# This is gross and another reason why depflags.pl must die.
+libc_dev_dep = $(libc) (= $(DEB_VERSION)),
+libc_dev_control_flags := $(subst Depends=",Depends="$(libc_dev_dep) ,$(libc_dev_control_flags))
 
 $(libc)_control_flags = $(libc_control_flags)
 $(libc)-dev_control_flags = $(libc_dev_control_flags)
