768417f47940fb97df634c6c27181294dc5464b8
Jeff Bailey <jbailey@debian.org>
2003-10-25 15:43:20 +0000

    - debian/sysdeps/kfreebsd-gnu.mk: New file.  Put in all the magic       from the old packaging.     - debian/sysdeps/kfreebsd.mk: Deleted.     - debian/sysdeps/gnu.mk: Add in GLIBC_OVERLAYS to allow patching to work,       create ld.so symlink at end of build.
    svn path=/branches/nptl/; revision=518

--- debian/sysdeps/gnu.mk
+++ debian/sysdeps/gnu.mk
@@ -1,5 +1,16 @@
 # This is for the GNU OS.  Commonly known as the Hurd.
 
+# FIXME: Have to do both nptl and linuxthreads for now, because of the
+# patch system.  Shouldn't need either of these.
+GLIBC_OVERLAYS ?= $(shell ls nptl* glibc-linuxthreads*)
+
 threads = no
 libc = libc0.3
 slibdir = /lib
+
+# Glibc should really do this for us.
+define libc_extra_install
+mkdir -p debian/tmp-$(curpass)/lib
+ln -s ld.so.1 /lib/ld.so debian/tmp-$(curpass)/lib/ld.so
+endef
+
