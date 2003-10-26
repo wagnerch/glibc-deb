7f5e575e1781c6b22bf012bdca2d700a10d23231
Jeff Bailey <jbailey@debian.org>
2003-10-26 16:50:00 +0000

    * Jeff Bailey <jbailey@nisa.net>
        - debian/sysdeps/linux.mk: Use getconf _NPROCESSORS_ONLN instead
          of /proc/cpuinfo to calculate NJOBS.  The cpuinfo file format
          is not consistent between arch's.
        - debian/.cvsignore: New file.
    
    svn path=/branches/nptl/; revision=538

--- debian/sysdeps/linux.mk
+++ debian/sysdeps/linux.mk
@@ -3,7 +3,16 @@ MIN_KERNEL_SUPPORTED := 2.2.0
 libc = libc6
 
 # Support multiple makes at once based on number of processors
-NJOBS=$(shell if [ -f /proc/cpuinfo ] ; then echo `cat /proc/cpuinfo | grep 'processor' | wc -l` ; else echo 1 ; fi)
+# Common wisdom says parallel make can be up to 2n+1.
+# Should we do that to get faster builds?
+NJOBS:=$(shell getconf _NPROCESSORS_ONLN 2>/dev/null || echo 1)
+ifeq ($(NJOBS),-1)
+ NJOBS:=1
+endif
+
+ifeq ($(NJOBS),0)
+ NJOBS=1
+endif
 
 # Linuxthreads Config
 threads = yes
