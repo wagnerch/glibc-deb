088b6a448b6bbe5ff188c669d8449d18d6840763
Daniel Jacobowitz <dan@debian.org>
2003-10-26 16:24:09 +0000

    Start -8.     - Don't use --enable-omitfp since we don't install libc_g.a.     - Enable NPTL for S/390.
    svn path=/branches/nptl/; revision=537

--- debian/sysdeps/s390.mk
+++ debian/sysdeps/s390.mk
@@ -1,4 +1,4 @@
-GLIBC_PASSES += s390x
+GLIBC_PASSES += nptl s390x
 DEB_ARCH_REGULAR_PACKAGES += libc6-s390x libc6-dev-s390x
 
 s390x_MIN_KERNEL_SUPPORTED = 2.4.1
@@ -6,10 +6,9 @@ s390x_configure_target = s390x-linux
 s390x_CC = $(CC) -m64
 s390x_add-ons = linuxthreads $(add-ons)
 libc6-s390x_shlib_dep = libc6-s390x (>= $(shlib_dep_ver)
-s390x_extra_cflags = -g0
+s390x_extra_cflags = -g0 -O3 -fomit-frame-pointer -D__USE_STRING_INLINES
 s390x_LIBDIR = 64
-
-extra_config_options := --enable-omitfp
+s390x_extra_config_options := $(extra_config_options) --disable-profile
 
 define s390x_extra_install
 mkdir -p debian/tmp-$(curpass)/lib
