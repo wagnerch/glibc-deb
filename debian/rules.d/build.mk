7b0a813fb1c5628ba25b78e854496d22e9cbf284
Denis Barbier <barbier@debian.org>
2006-05-04 20:52:54 +0000

    Port r1417 and r1443 from trunk to add a locales-all package
    svn path=/glibc-package/branches/glibc-2.4/; revision=1466

--- debian/rules.d/build.mk
+++ debian/rules.d/build.mk
@@ -104,6 +104,10 @@ $(stamp)install_%: $(stamp)check_%
 	if [ $(curpass) = libc ]; then \
 	  $(MAKE) -f debian/generate-supported.mk IN=$(DEB_SRCDIR)/localedata/SUPPORTED \
 	    OUT=debian/tmp-$(curpass)/usr/share/i18n/SUPPORTED; \
+	  $(MAKE) -C $(DEB_SRCDIR)/localedata -j $(NJOBS) \
+	    objdir=$(DEB_BUILDDIR) install_root=$(CURDIR)/debian/tmp-$(curpass) \
+	    install-locales; \
+	  (cd $(DEB_SRCDIR)/manual && texi2html -split_chapter libc.texinfo); \
 	fi
 
 	# Remove ld.so from optimized libraries
