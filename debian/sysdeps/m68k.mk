978f7294cb8906d1c07d7cd844a2ea0cac69522e
Clint Adams <schizo@debian.org>
2006-05-23 01:45:24 +0000

    and m68k
    svn path=/glibc-package/branches/glibc-2.4/; revision=1514

--- debian/sysdeps/m68k.mk
+++ debian/sysdeps/m68k.mk
@@ -3,8 +3,4 @@ MIN_KERNEL_SUPPORTED := 2.2.0
 
 udeb_MIN_KERNEL_SUPPORTED = 2.4.1
 
-# work around to build on m68k, due to gcc-4.0 ICE.  See #319312.
-CC = gcc-3.4
-BUILD_CC = gcc-3.4
-
 libc_extra_config_options = $(extra_config_options) --without-__thread --disable-sanity-checks
