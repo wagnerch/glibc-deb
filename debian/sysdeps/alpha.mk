03fed9bdf008be8064b658fbb83ca7c7e85575ee
Aurelien Jarno <aurel32@debian.org>
2006-05-30 20:17:07 +0000

    * Add patches/alpha/local-gcc4.1.diff (fix build with gcc-4.1) from     Falk Hueffner.
    svn path=/glibc-package/branches/glibc-2.4/; revision=1590

--- debian/sysdeps/alpha.mk
+++ debian/sysdeps/alpha.mk
@@ -4,10 +4,6 @@ libc = libc6.1
 # even if TIMEOUTSCALE is set - SIGALRM from the program is just ignored.
 RUN_TESTSUITE = no
 
-# work around to build on alpha, due to gcc-4.1 generating bad instructions.
-CC = gcc-4.0
-BUILD_CC = gcc-4.0
-
 # disabled for static linked pthread programs.
 libc_extra_config_options = $(extra_config_options) --without-__thread --disable-sanity-checks
 
