6e75d4f1954360068da74c0bcefc38f535bf585d
Jeff Bailey <jbailey@debian.org>
2006-06-06 21:25:34 +0000

    Make sure to include the line to tell glibc where to install the header files
    svn path=/glibc-package/branches/glibc-2.4/; revision=1624

--- debian/sysdeps/powerpc.mk
+++ debian/sysdeps/powerpc.mk
@@ -12,7 +12,8 @@ libc6-ppc64_shlib_dep = libc6-ppc64 (>= $(shlib_dep_ver))
 ppc64_extra_cflags = -O3 -g1
 ppc64_slibdir = /lib64
 ppc64_libdir = /usr/lib64
-ppc64_extra_config_options := $(extra_config_options) --disable-profile
+ppc64_extra_config_options := $(extra_config_options) --disable-profile \
+	--includedir=/usr/include/ppc64-linux-gnu
 
 define libc6-dev-ppc64_extra_pkg_install
 mkdir -p debian/libc6-dev-ppc64/usr/include
