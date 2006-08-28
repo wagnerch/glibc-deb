5e758c1422bca25e910f188d2daff6b07e2c9e1f
Aurelien Jarno <aurel32@debian.org>
2006-08-28 04:41:11 +0000

    * rules.d/debhelper.mk: don't make ld.so.8.gz or ld.so.conf executable.
    svn path=/glibc-package/branches/glibc-2.4/; revision=1796

--- debian/rules.d/debhelper.mk
+++ debian/rules.d/debhelper.mk
@@ -116,9 +116,9 @@ endif
 	# an unescaped regular expression.  ld.so must be executable;
 	# libc.so and NPTL's libpthread.so print useful version
 	# information when executed.
-	find debian/$(curpass) -type f \( -regex '.*lib.*/ld.*so.*' \
-		-o -regex '.*lib.*/libpthread.*so.*' \
-		-o -regex '.*lib.*/libc[.-].*so.*' \) \
+	find debian/$(curpass) -type f \( -regex '.*lib[0-9]*/ld.*so.*' \
+		-o -regex '.*lib[0-9]*/libpthread.*so.*' \
+		-o -regex '.*lib[0-9]*/libc[.-].*so.*' \) \
 		-exec chmod a+x '{}' ';'
 	dh_makeshlibs -p$(curpass) -V "$(call xx,shlib_dep)"
 
@@ -143,9 +143,9 @@ $(patsubst %,$(stamp)binaryinst_%,$(DEB_UDEB_PACKAGES)): $(stamp)debhelper
 
 	dh_compress -p$(curpass)
 	dh_fixperms -p$(curpass)
-	find debian/$(curpass) -type f \( -regex '.*lib.*/ld.*so.*' \
-		-o -regex '.*lib.*/tls/.*libpthread.*so.*' \
-		-o -regex '.*lib.*/libc[.-].*so.*' \) \
+	find debian/$(curpass) -type f \( -regex '.*lib[0-9]*/ld.*so.*' \
+		-o -regex '.*lib[0-9]*/.*libpthread.*so.*' \
+		-o -regex '.*lib[0-9]*/libc[.-].*so.*' \) \
 		-exec chmod a+x '{}' ';'
 	# dh_makeshlibs -p$(curpass) -V "$(call xx,shlib_dep)"
 	dh_installdeb -p$(curpass)
