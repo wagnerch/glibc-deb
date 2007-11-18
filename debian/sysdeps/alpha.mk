228b230a5df1472f8738989161ee84348254e6e5
Aurelien Jarno <aurel32@debian.org>
2007-11-18 12:30:40 +0000

    * Add an ev67 flavour on alpha, but keep it disabled for now:     - control.in/opt: add libc6-ev67 packages.     - sysdeps/alpha.mk: add a new pass for ev67 flavour.
    svn path=/glibc-package/branches/glibc-2.7/; revision=2681

--- debian/sysdeps/alpha.mk
+++ debian/sysdeps/alpha.mk
@@ -3,3 +3,12 @@ libc = libc6.1
 # Requires Linux 2.6.9 for NPTL
 libc_MIN_KERNEL_SUPPORTED = 2.6.9
 
+# build an ev67 optimized library
+#GLIBC_PASSES += ev67
+#DEB_ARCH_REGULAR_PACKAGES += libc6.1-ev67
+ev67_add-ons = nptl $(add-ons)
+ev67_configure_target = alphaev67-linux
+ev67_extra_cflags = -mcpu=ev67 -mtune=ev67 -g -O3
+ev67_extra_config_options = $(extra_config_options) --disable-profile
+ev67_rltddir = /lib/ev67
+ev67_slibdir = /lib/ev67
