e407253fffef1db46ff089c2f9cc76eecb2c6fd3
Aurelien Jarno <aurel32@debian.org>
2007-11-18 18:43:42 +0000

    Rename libc6.1-ev67 into libc6-alphaev67 to match glibc name, and enable this flavour
    svn path=/glibc-package/branches/glibc-2.7/; revision=2682

--- debian/sysdeps/alpha.mk
+++ debian/sysdeps/alpha.mk
@@ -4,11 +4,11 @@ libc = libc6.1
 libc_MIN_KERNEL_SUPPORTED = 2.6.9
 
 # build an ev67 optimized library
-#GLIBC_PASSES += ev67
-#DEB_ARCH_REGULAR_PACKAGES += libc6.1-ev67
-ev67_add-ons = nptl $(add-ons)
-ev67_configure_target = alphaev67-linux
-ev67_extra_cflags = -mcpu=ev67 -mtune=ev67 -g -O3
-ev67_extra_config_options = $(extra_config_options) --disable-profile
-ev67_rltddir = /lib/ev67
-ev67_slibdir = /lib/ev67
+GLIBC_PASSES += alphaev67
+DEB_ARCH_REGULAR_PACKAGES += libc6.1-alphaev67
+alphaev67_add-ons = nptl $(add-ons)
+alphaev67_configure_target = alphaev67-linux
+alphaev67_extra_cflags = -mcpu=ev67 -mtune=ev67 -g -O2
+alphaev67_extra_config_options = $(extra_config_options) --disable-profile
+alphaev67_rltddir = /lib/ev67
+alphaev67_slibdir = /lib/ev67
