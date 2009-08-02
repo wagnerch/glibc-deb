68953698f79b8ee4ce5bc875e8817d6df79e0119
Aurelien Jarno <aurel32@debian.org>
2009-08-02 23:46:51 +0000

    Don't build biarch/optimized libraries with -O3
    svn path=/glibc-package/branches/eglibc-2.10/; revision=3731

--- debian/sysdeps/i386.mk
+++ debian/sysdeps/i386.mk
@@ -5,7 +5,7 @@ EGLIBC_PASSES += i686
 DEB_ARCH_REGULAR_PACKAGES += libc6-i686
 i686_add-ons = nptl $(add-ons)
 i686_configure_target=i686-linux
-i686_extra_cflags = -march=i686 -mtune=i686 -O3
+i686_extra_cflags = -march=i686 -mtune=i686
 i686_rtlddir = /lib
 i686_slibdir = /lib/i686/cmov
 i686_extra_config_options = $(extra_config_options) --disable-profile
@@ -16,7 +16,7 @@ EGLIBC_PASSES += xen
 DEB_ARCH_REGULAR_PACKAGES += libc6-xen
 xen_add-ons = nptl $(add-ons)
 xen_configure_target=i686-linux
-xen_extra_cflags = -march=i686 -mtune=i686 -O3 -mno-tls-direct-seg-refs
+xen_extra_cflags = -march=i686 -mtune=i686 -mno-tls-direct-seg-refs
 xen_rtlddir = /lib
 xen_slibdir = /lib/i686/nosegneg
 xen_extra_config_options = $(extra_config_options) --disable-profile
@@ -41,7 +41,6 @@ amd64_configure_target = x86_64-linux
 # /usr/include/asm wrappers need that symbol.
 amd64_CC = $(CC) -m64 -D__x86_64__
 amd64_CXX = $(CXX) -m64 -D__x86_64__
-amd64_extra_cflags = -O3
 amd64_extra_config_options = $(extra_config_options) --disable-profile
 amd64_slibdir = /lib64
 amd64_libdir = /usr/lib64
