ce82f9ab4d881ad0c11092338d12d446078667c9
Aurelien Jarno <aurel32@debian.org>
2009-08-23 21:29:53 +0000

    Install the libc in the multiarch path
    svn path=/glibc-package/branches/multiarch/; revision=3780

--- debian/rules.d/build.mk
+++ debian/rules.d/build.mk
@@ -148,7 +148,6 @@ $(stamp)install_%: $(stamp)check_%
 	  machine=`sed '/^ *config-machine *=/!d;s/.*= *//g' $(DEB_BUILDDIR)/config.make`; \
 	  os=`sed '/^ *config-os *=/!d;s/.*= *//g' $(DEB_BUILDDIR)/config.make`; \
 	  triplet="$$machine-$$os"; \
-	  mkdir -p debian/tmp-$(curpass)/lib/$$triplet debian/tmp-$(curpass)/usr/lib/$$triplet; \
 	  conffile="debian/tmp-$(curpass)/etc/ld.so.conf.d/$$triplet.conf"; \
 	  echo "# Multiarch support" > $$conffile; \
 	  echo /lib/$$triplet >> $$conffile; \
--- debian/rules.d/debhelper.mk
+++ debian/rules.d/debhelper.mk
@@ -157,11 +157,11 @@ $(stamp)debhelper:
 	      ;; \
 	    debian/$(libc).preinst) \
 	      rtld=`LANG=C LC_ALL=C readelf -l debian/tmp-libc/usr/bin/iconv | grep "interpreter" | sed -e 's/.*interpreter: \(.*\)]/\1/g'`; \
-	      c_so=`ls debian/tmp-libc/lib/ | grep "libc\.so\."` ; \
-	      m_so=`ls debian/tmp-libc/lib/ | grep "libm\.so\."` ; \
-	      pthread_so=`ls debian/tmp-libc/lib/ | grep "libpthread\.so\."` || true; \
-	      rt_so=`ls debian/tmp-libc/lib/ | grep "librt\.so\."` ; \
-	      dl_so=`ls debian/tmp-libc/lib/ | grep "libdl\.so\."` ; \
+	      c_so=`ls debian/tmp-libc/$(slibdir) | grep "libc\.so\."` ; \
+	      m_so=`ls debian/tmp-libc/$(slibdir) | grep "libm\.so\."` ; \
+	      pthread_so=`ls debian/tmp-libc/$(slibdir) | grep "libpthread\.so\."` || true; \
+	      rt_so=`ls debian/tmp-libc/$(slibdir) | grep "librt\.so\."` ; \
+	      dl_so=`ls debian/tmp-libc/$(slibdir) | grep "libdl\.so\."` ; \
 	      sed -e "s#RTLD#$$rtld#" -e "s#C_SO#$$c_so#" -e "s#M_SO#$$m_so#" -e "s#PTHREAD_SO#$$pthread_so#" -e "s#RT_SO#$$rt_so#" -e "s#DL_SO#$$dl_so#" -i $$z ; \
 	      ;; \
 	  esac; \
