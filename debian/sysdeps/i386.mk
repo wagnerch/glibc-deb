5ab7b03d4b7b321b0a6d0e5b7536b8042ca8cbb2
Aurelien Jarno <aurel32@debian.org>
2009-09-09 19:20:58 +0000

    Really fix Xen lib installation directory
    svn path=/glibc-package/branches/multiarch/; revision=3831

--- debian/sysdeps/i386.mk
+++ debian/sysdeps/i386.mk
@@ -55,8 +55,8 @@ endef
 
 define libc6-dev_extra_pkg_install
 mkdir -p debian/libc6-dev/usr/lib/xen
-cp -af debian/tmp-xen/$(slibdir)/*.a \
-	debian/libc6-dev/$(slibdir)/xen
+cp -af debian/tmp-xen/$(libdir)/*.a \
+	debian/libc6-dev/$(libdir)/xen
 endef
 
 define libc6-dev-amd64_extra_pkg_install
