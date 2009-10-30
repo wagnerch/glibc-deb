ebdd258f72b5cdbd6767415e24d9838cc53f82c0
Aurelien Jarno <aurel32@debian.org>
2009-10-30 20:14:01 +0000

    Tag eglibc 2.10.1-4
    svn path=/glibc-package/tags/2.10.1-4/; revision=3917

--- debian/rules.d/build.mk
+++ debian/rules.d/build.mk
@@ -33,6 +33,7 @@ $(stamp)configure_%: $(stamp)mkbuilddir_%
 	echo "KSH := /bin/bash"			>> $(DEB_BUILDDIR)/configparms
 	echo "SHELL := /bin/bash"		>> $(DEB_BUILDDIR)/configparms
 	echo "LIBGD = no"			>> $(DEB_BUILDDIR)/configparms
+	echo "have-fpie = $(fpie)"              >> $(DEB_BUILDDIR)/configparms
 	echo "bindir = $(bindir)"		>> $(DEB_BUILDDIR)/configparms
 	echo "datadir = $(datadir)"		>> $(DEB_BUILDDIR)/configparms
 	echo "localedir = $(localedir)" 	>> $(DEB_BUILDDIR)/configparms
@@ -159,7 +160,8 @@ $(stamp)doc: $(stamp)patch
 	touch $@
 
 $(stamp)source: $(stamp)patch
-	tar -c --lzma -C .. \
-		-f $(build-tree)/eglibc-$(EGLIBC_VERSION).tar.lzma \
+	mkdir -p $(build-tree)
+	tar -c -J -C .. \
+		-f $(build-tree)/eglibc-$(EGLIBC_VERSION).tar.xz \
 		$(EGLIBC_SOURCES)
 	touch $@
