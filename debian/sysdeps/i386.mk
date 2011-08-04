f60ee57ed15c9b50ef91ba9f8d35278a59886d7d
Aurelien Jarno <aurel32@debian.org>
2011-08-04 21:59:13 +0000

    Tags eglibc 2.13-14
    svn path=/glibc-package/tags/2.13-14/; revision=4849

--- debian/sysdeps/i386.mk
+++ debian/sysdeps/i386.mk
@@ -46,14 +46,6 @@ amd64_rtlddir = /lib64
 amd64_slibdir = /lib64
 amd64_libdir = /usr/lib64
 
-define amd64_extra_install
-cp debian/tmp-amd64/usr/bin/ldd \
-	debian/tmp-libc/usr/bin
-cp -af debian/tmp-amd64/usr/include/* \
-	debian/tmp-libc/usr/include
-rm -f debian/tmp-libc/usr/include/gnu/stubs-64.h
-endef
-
 define libc6-dev_extra_pkg_install
 mkdir -p debian/libc6-dev/$(libdir)/xen
 cp -af debian/tmp-xen/$(libdir)/*.a \
@@ -61,12 +53,20 @@ cp -af debian/tmp-xen/$(libdir)/*.a \
 endef
 
 define libc6-dev-amd64_extra_pkg_install
+
+mkdir -p debian/libc6-dev-amd64/usr/include
+ln -s i386-linux-gnu/bits debian/libc6-dev-amd64/usr/include/
+ln -s i386-linux-gnu/gnu debian/libc6-dev-amd64/usr/include/
+ln -s i386-linux-gnu/fpu_control.h debian/libc6-dev-amd64/usr/include/
+
 mkdir -p debian/libc6-dev-amd64/usr/include/i386-linux-gnu/gnu
-cp -af debian/tmp-amd64/usr/include/gnu/stubs-64.h \
+cp -a debian/tmp-amd64/usr/include/gnu/stubs-64.h \
         debian/libc6-dev-amd64/usr/include/i386-linux-gnu/gnu
-ln -s i386-linux-gnu/gnu debian/libc6-dev-amd64/usr/include/gnu
-ln -s i386-linux-gnu/sys debian/libc6-dev-amd64/usr/include/sys
-ln -s i386-linux-gnu/bits debian/libc6-dev-amd64/usr/include/bits
-mkdir -p debian/libc6-dev-amd64/usr/include/x86_64-linux-gnu
+
+mkdir -p debian/libc6-dev-amd64/usr/include/sys
+for i in `ls debian/tmp-libc/usr/include/i386-linux-gnu/sys` ; do \
+        ln -s ../i386-linux-gnu/sys/$$i debian/libc6-dev-amd64/usr/include/sys/$$i ; \
+done
+
 endef
 
