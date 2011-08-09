39aa62ebc18bd47c2231cc3c387453fbe1fe5a26
Aurelien Jarno <aurel32@debian.org>
2011-08-09 09:39:46 +0000

    Tag eglibc 2.13-16
    svn path=/glibc-package/tags/2.13-16/; revision=4861

--- debian/sysdeps/sparc.mk
+++ debian/sysdeps/sparc.mk
@@ -9,17 +9,24 @@ sparc64_CC = $(CC) -m64
 sparc64_CXX = $(CXX) -m64
 libc6-sparc64_shlib_dep = libc6-sparc64 (>= $(shlib_dep_ver))
 sparc64_extra_config_options = $(extra_config_options) --disable-profile
-sparc64_includedir = /usr/include/sparc64-linux-gnu
 sparc64_rtlddir = /lib64
 sparc64_slibdir = /lib64
 sparc64_libdir = /usr/lib64
 
 define libc6-dev-sparc64_extra_pkg_install
+
+mkdir -p debian/libc6-dev-sparc64/usr/include
+ln -s sparc-linux-gnu/bits debian/libc6-dev-sparc64/usr/include/
+ln -s sparc-linux-gnu/gnu debian/libc6-dev-sparc64/usr/include/
+ln -s sparc-linux-gnu/fpu_control.h debian/libc6-dev-sparc64/usr/include/
+
 mkdir -p debian/libc6-dev-sparc64/usr/include/sparc-linux-gnu/gnu
-cp -af debian/tmp-sparc64/usr/include/sparc64-linux-gnu/gnu/stubs-64.h \
+cp -a debian/tmp-sparc64/usr/include/gnu/stubs-64.h \
         debian/libc6-dev-sparc64/usr/include/sparc-linux-gnu/gnu
-ln -s sparc-linux-gnu/gnu debian/libc6-dev-sparc64/usr/include/gnu
-ln -s sparc-linux-gnu/sys debian/libc6-dev-sparc64/usr/include/sys
-ln -s sparc-linux-gnu/bits debian/libc6-dev-sparc64/usr/include/bits
-mkdir -p debian/libc6-dev-sparc64/usr/include/sparc64-linux-gnu
+
+mkdir -p debian/libc6-dev-sparc64/usr/include/sys
+for i in `ls debian/tmp-libc/usr/include/sparc-linux-gnu/sys` ; do \
+        ln -s ../sparc-linux-gnu/sys/$$i debian/libc6-dev-sparc64/usr/include/sys/$$i ; \
+done
+
 endef
