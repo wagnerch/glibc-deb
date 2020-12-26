7e27e7bb8d1d152ee9919a4e6e097e9adbaa73e9
Samuel Thibault <samuel.thibault@ens-lyon.org>
2020-12-26 16:41:14 +0100

    debian/testsuite-xfail-debian.mk: Update with 2.33 tests

--- debian/testsuite-xfail-debian.mk
+++ debian/testsuite-xfail-debian.mk
@@ -333,7 +333,6 @@ test-xfail-tst-gmon-static-gprof = yes
 test-xfail-tst-tls1-static-non-pie = yes
 test-xfail-tst-libc_dlvsym-static = yes
 test-xfail-tst-libc_dlvsym = yes
-test-xfail-tst-spawn4-compat = yes
 
 # want /proc/self/fd
 test-xfail-tst-if_index-long = yes
@@ -391,6 +390,7 @@ test-xfail-test-fenv-sse-2 = yes
 test-xfail-test-fesetexcept-traps = yes
 test-xfail-tst-ptsname = yes
 test-xfail-tst-spawn4 = yes
+test-xfail-tst-spawn4-compat = yes
 
 # actually never succeded
 test-xfail-tst-create_format1 = yes
