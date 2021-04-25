d430db6224603b9e2eb55de11fd9a0f9a61676ec
Aurelien Jarno <aurelien@aurel32.net>
2021-04-25 18:49:38 +0200

    debian/patches/any/local-rtlddir-cross.diff: drop patch, letting upstream makefiles to install the dynamic linker symlink directly in the right location. This fixes the temporary installation done by upstream makefiles to run some tests in a container.  Closes: #973278, #985617.
    * debian/patches/any/local-rtlddir-cross.diff: drop patch, letting upstream
      makefiles to install the dynamic linker symlink directly in the right
      location. This fixes the temporary installation done by upstream makefiles
      to run some tests in a container.  Closes: #973278, #985617.
    * debian/rules.d/build.mk: do not create the dynamic linker manually.
    * debian/sysdeps/*.mk: do not create the dynamic linker manually for
      bi/tri-arch packages.
    * debian/rules.d/build.mk: create the soname symlink for ld-2.xx.so, to
      avoid its creation later by ldconfig.
    * debian/debhelper.in/libc.install, debhelper.in/libc-alt.install,
      debhelper.in/libc-udeb.install, debhelper.in/libc-udeb.install.hurd-i386:
      adjust given that the dynamic linker symlink is now already at the correct
      location.

--- debian/sysdeps/hurd-i386.mk
+++ debian/sysdeps/hurd-i386.mk
@@ -16,8 +16,6 @@ endif
 
 # Glibc should really do this ld.so link for us.
 define libc_extra_install
-mkdir -p debian/tmp-$(curpass)/lib
-ln -s ld.so.1 debian/tmp-$(curpass)/lib/ld.so
 mkdir -p debian/tmp-$(curpass)/usr/include/$(DEB_HOST_MULTIARCH)/mach
 mv debian/tmp-$(curpass)/usr/include/mach/i386 debian/tmp-$(curpass)/usr/include/$(DEB_HOST_MULTIARCH)/mach/
 ln -s ../$(DEB_HOST_MULTIARCH)/mach/i386 debian/tmp-$(curpass)/usr/include/mach/i386
