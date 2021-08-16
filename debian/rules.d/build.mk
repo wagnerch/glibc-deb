05169cfde53889fdd4a3ad0c457b07f198ac118c
Aurelien Jarno <aurelien@aurel32.net>
2021-08-16 23:29:15 +0200

    debian/rules.d/build.mk: stop passing --enable-obsolete-rpc.
    * debian/rules.d/build.mk: stop passing --enable-obsolete-rpc.
    * debian/debhelper.in/libc-dev.install{,.hurd-i386}: do not install
      librpcsvc.a.
    * debian/debhelper.in/libc-dev-bin.manpage, debian/local/manpages/rpcgen.1:
      do not install rpcgen (1) manpage.
    * debian/rules.d/build.mk: stop deleting <rpcsvc/yppasswd.h> and
      <rpcsvc/yppasswd.x>.
    * debian/control.in/libc, debian/rules.d/debhelper.mk: make libc6-dev to
      depend on rpcsvc-proto, except for stage1 and stage2.

--- debian/rules.d/build.mk
+++ debian/rules.d/build.mk
@@ -109,7 +109,6 @@ endif
 		--without-selinux \
 		--enable-stackguard-randomization \
 		--enable-stack-protector=strong \
-		--enable-obsolete-rpc \
 		--with-pkgversion="Debian GLIBC $(DEB_VERSION)" \
 		--with-bugurl="http://www.debian.org/Bugs/" \
 		$(if $(filter $(pt_chown),yes),--enable-pt_chown) \
@@ -262,11 +261,6 @@ else
 	  $(MAKE) -f debian/generate-supported.mk IN=localedata/SUPPORTED \
 	    OUT=debian/tmp-$(curpass)/usr/share/i18n/SUPPORTED; \
 	fi
-
-	# Remove yppasswd.h and yppasswd.x as they are shipped by libnsl-dev
-	# This could probably be removed once we stop using --enable-obsolete-rpc.
-	rm $(CURDIR)/debian/tmp-$(curpass)/usr/include/rpcsvc/yppasswd.h
-	rm $(CURDIR)/debian/tmp-$(curpass)/usr/include/rpcsvc/yppasswd.x
 
 ifeq ($(DEB_HOST_ARCH_OS),linux)
 	# Install the Python pretty printers
